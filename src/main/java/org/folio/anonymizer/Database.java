8421b72c9e6257ae18b7ed951f3d97b60b1fd4ac
Kriti Jain <kjain@ebsco.com>
2024-09-10 12:33:39 -0500

    adding variables to db class

--- src/main/java/org/folio/anonymizer/Database.java
+++ src/main/java/org/folio/anonymizer/Database.java
@@ -1,3 +1,14 @@
 package org.folio.anonymizer;
 
-public class Database {}
+public class Database {
+    public static final String SCHEMA_NAME = "${tenant}_mod_users";
+    public static final String TABLE_NAME = "users";
+
+    public static String getSchemaName() {
+        return SCHEMA_NAME;
+    }
+
+    public static String getTableName() {
+        return TABLE_NAME;
+    }
+}
