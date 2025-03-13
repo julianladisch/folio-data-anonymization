9fe6f11a4031642cc1e183ba55c19ef89b9df7d6
Shelley Doljack <sdoljack@stanford.edu>
2025-03-13 17:34:31 -0600

    Consolidates tests into one file for pytest_postgresql fixtures are session scoped.

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,7 +3,7 @@ import psycopg
 import os
 from pytest_postgresql import factories
 from folio_data_anonymization.database import Database
-from tests.mock_db import fixture_db
+from folio_data_anonymization.users import Users
 
 
 @pytest.fixture
@@ -18,7 +18,27 @@ def env_vars() -> dict:
     }
 
 
-postgresql = factories.postgresql(process_fixture_name="fixture_db")
+def load_database(**kwargs):
+    db_connection = psycopg.connect(**kwargs)
+    with db_connection.cursor() as cur:
+        cur.execute("CREATE TABLE users (id serial PRIMARY KEY, name varchar);")
+        cur.execute(
+            "INSERT INTO users (name) VALUES"
+            "('John Doe'), ('Jane Doe'), ('Mary Smith');"
+        )
+        db_connection.commit()
+
+
+postgresql_proc = factories.postgresql_proc(
+    host=os.getenv("PGHOST"),
+    port=os.getenv("PGPORT"),
+    user=os.getenv("PGUSER"),
+    password=os.getenv("PGPASSWORD"),
+    dbname=os.getenv("PGDATABASE"),
+    load=[load_database],
+)
+
+postgresql = factories.postgresql(process_fixture_name="postgresql_proc")
 
 
 def test_load_env_vars_pytest_env(env_vars):
@@ -56,3 +76,10 @@ def test_table_name(env_vars, postgresql):
     db = Database()
     name = db.table_name('mod_users', 'users')
     assert name == 'diku_mod_users.users'
+
+
+def test_anonymize_users(mocker, postgresql, caplog):
+    mocker.patch("folio_data_anonymization.users.Users", return_value=postgresql)
+    users_task = Users()
+    users_task.anonymize_users()
+    assert "Anonymizing diku_mod_users.users" in caplog.text
