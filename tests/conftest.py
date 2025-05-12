782174c8c1faaadae959c8893f1178d0517fce96
Jeremy Nelson <jermnelson@gmail.com>
2025-05-12 16:51:29 -0700

    feat: Tests fake_jsonb function

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,19 @@
+import json
 import pathlib
 import sys
 
+import pytest
 
 root_directory = pathlib.Path(__file__).parent.parent
 dir = root_directory
 
 sys.path.append(str(dir))
+
+
+@pytest.fixture
+def configurations():
+    configuration = {}
+    for file_path in (root_directory / "plugins/config").glob("*.json"):
+        config_contents = json.loads(file_path.read_text())
+        configuration[file_path.stem] = config_contents
+    return configuration
---
+++ tests/test_utils.py
@@ -0,0 +1,27 @@
+import copy
+import json
+import pathlib
+
+from folio_data_anonymization.plugins.utils import fake_jsonb
+
+
+def test_org_fake_jsonb(configurations):
+    with (pathlib.Path(__file__).parent / "examples/organization.json").open() as fo:
+        organization = json.load(fo)
+
+    original_org = copy.deepcopy(organization)
+    for row in configurations.get("anonymize_organization_tables"):
+        if row['table_name'].startswith("mod_organizations_storage.organizations"):
+            org_config = row
+
+    fake_jsonb(organization, org_config)
+
+    assert organization["name"] != original_org["name"]
+    assert organization["edi"]["libEdiType"] != original_org["edi"]["libEdiType"]
+    assert (
+        organization["addresses"][0]["addressLine1"]
+        != original_org["addresses"][0]["addressLine1"]
+    )
+    assert organization["addresses"][0]["city"] != original_org["addresses"][0]["city"]
+    assert organization["emails"][0]["value"] != original_org["emails"][0]["value"]
+    assert len(organization["aliases"][0]["description"]) == 0
