41c300116e8c5abe2ae8678155c4b775b0e0bb70
Joshua Greben <jgreben@stanford.edu>
2025-05-16 12:01:20 -0700

    Update plugins config location for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,14 +6,14 @@ import pytest
 
 root_directory = pathlib.Path(__file__).parent.parent
 dir = root_directory
-
 sys.path.append(str(dir))
 
 
 @pytest.fixture
 def configurations():
     configuration = {}
-    for file_path in (root_directory / "folio_data_anonymization/plugins/config ").glob("*.json"):
+    config_path = root_directory / "folio_data_anonymization/plugins/config"
+    for file_path in config_path.glob("*.json"):
         config_contents = json.loads(file_path.read_text())
         configuration[file_path.stem] = config_contents
     return configuration
